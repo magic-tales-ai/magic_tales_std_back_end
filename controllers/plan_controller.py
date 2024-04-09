80356cf5078fc743ea39b35c92b49f90e575d127
KevinSieb <kevinsiebenhar7@gmail.com>
2024-04-09 13:24:25 -0300

    feat: upload and read profiles images endpoints; read plans images endpoint

--- controllers/plan_controller.py
+++ controllers/plan_controller.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, status, Depends, HTTPException
+from fastapi.responses import FileResponse
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.future import select  # Use future select for compatibility with async
 from db import get_session, AsyncSession
 from models.db.plan import Plan
 import logging
+import os
 
 from utils.log_utils import get_logger
 
@@ -42,3 +44,39 @@ async def get(session: AsyncSession = Depends(get_session)):
         # Log the specific database error and raise a 500 HTTPException
         logger.error(f"Database error while fetching plans: {e}")
         raise HTTPException(status_code=500, detail="Database error occurred")
+    
+@plan_router.get("/{id}/image", status_code=status.HTTP_200_OK)
+async def get_image_by_plan_id(id: int, session: AsyncSession = Depends(get_session)):
+    """
+    Asynchronusly get a plan image file by its ID
+
+    Args:
+        id (int): The ID of the plan.
+        session (AsyncSession): The database session used for the operation.
+
+    Returns:
+        FileResponse: The plan image file
+    """
+    try:
+        plan = await session.get(Plan, id)
+        plan_images_folder = os.getenv("STATIC_FOLDER") + "/plans"
+        
+        if plan is None:
+            logger.info(f"Plan with ID: {id} not found")
+            raise HTTPException(status_code=404, detail="Plan not found")
+        
+        if not os.path.exists(plan_images_folder):
+            raise HTTPException(status_code=404, detail="Plans static folder doesn't exist")
+        
+        image_path = os.path.join(plan_images_folder, str(id) + ".png")
+        
+        if not os.path.exists(image_path):
+            raise HTTPException(status_code=404, detail="Image not found")
+        
+        return FileResponse(image_path)
+    except HTTPException as e:
+        logger.error(e.detail)
+        raise HTTPException(status_code=404, detail=e.detail)
+    except Exception as e:
+        logger.error(f"Images not found.")
+        raise HTTPException(status_code=404, detail="Images not found")
--- controllers/profile_controller.py
+++ controllers/profile_controller.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, status, Depends, HTTPException
+from fastapi import APIRouter, status, Depends, HTTPException, File, UploadFile
+from fastapi.responses import FileResponse
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
@@ -8,6 +9,7 @@ from services.session_service import check_token
 from models.db.profile import Profile
 from models.api.profile_api import ProfileAPI
 import logging
+import os
 
 from utils.log_utils import get_logger
 
@@ -74,6 +76,88 @@ async def get_by_id(
         logger.error(f"Failed to fetch profile by ID: {e}")
         raise HTTPException(status_code=500, detail="Database error occurred")
 
+
+@profile_router.post("/{id}/upload-image", status_code=status.HTTP_200_OK)
+async def upload_image_by_profile_id(
+    id: int,
+    image: UploadFile=File(...),
+    session: AsyncSession = Depends(get_session),
+    token_data: dict = Depends(check_token),
+):
+    """
+    Asynchronusly upload a profile image by its ID. If image already exists, will be replaced
+
+    Args:
+        id (int): The ID of the profile.
+        image (UploadFile, optional): The image to upload.
+        session (AsyncSession): The database session used for the operation.
+        token_data (dict): Token data for authentication and authorization.
+
+    Returns:
+        dict: A message indicating the outcome of the uploading attempt.
+    """
+    try:
+        profile = await session.get(Profile, id)
+        profiles_folder = os.getenv("STATIC_FOLDER") + "/profiles"
+        allowed_extensions = { '.jpg', '.jpeg', 'png' }
+        filename, ext = os.path.splitext(image.filename)
+        
+        if ext.lower() not in allowed_extensions:
+            raise HTTPException(status_code=400, detail="File must be .jpg, .jpeg or .png")
+        
+        if profile is None:
+            logger.info(f"Profile with ID: {id} not found")
+            raise HTTPException(status_code=404, detail="Profile not found")
+        
+        # If the folder "static/profiles" doesn't exists, we create
+        if not os.path.exists(profiles_folder):
+            os.makedirs(profiles_folder)
+        
+        # Save image (the filename will be the profile id)
+        with open(os.path.join(profiles_folder, str(id) + ".png"), "wb") as buffer:
+            buffer.write(image.file.read())
+            
+        return {"message": "Image upload successfully"}
+    except SQLAlchemyError as e:
+        logger.error(f"Failed to upload image: {e}")
+        # Rollback is handled automatically if an exception occurs within the with block
+        raise HTTPException(status_code=500, detail="Failed to upload image")
+    
+@profile_router.get("/{id}/image", status_code=status.HTTP_200_OK)
+async def get_image_by_profile_id(
+    id: int,
+    session: AsyncSession = Depends(get_session),
+    token_data: dict = Depends(check_token),
+):
+    """
+    Asynchronusly get a profile image file by its ID
+
+    Args:
+        id (int): The ID of the profile.
+        session (AsyncSession): The database session used for the operation.
+        token_data (dict): Token data for authentication and authorization.
+
+    Returns:
+        FileResponse: The profile image file
+    """
+    try:
+        profile = await session.get(Profile, id)
+        profiles_folder = os.getenv("STATIC_FOLDER") + "/profiles"
+        
+        if profile is None:
+            logger.info(f"Profile with ID: {id} not found")
+            raise HTTPException(status_code=404, detail="Profile not found")
+        
+        image_path = os.path.join(profiles_folder, str(id) + ".png")
+        
+        if not os.path.exists(image_path):
+            raise HTTPException(status_code=404, detail="Image not found")
+        
+        return FileResponse(image_path)
+    except SQLAlchemyError as e:
+        logger.error(f"Failed to get image: {e}")
+        # Rollback is handled automatically if an exception occurs within the with block
+        raise HTTPException(status_code=500, detail="Failed to get image")
 
 @profile_router.post("/", status_code=status.HTTP_201_CREATED)
 async def post(
