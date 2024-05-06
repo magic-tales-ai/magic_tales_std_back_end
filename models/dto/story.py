ae1f9357cb64ef19b740aeaca222f985383e2571
KevinSieb <kevinsiebenhar7@gmail.com>
2024-05-06 09:34:25 -0300

    fix: story DTO model

--- models/dto/story.py
+++ models/dto/story.py
@@ -7,9 +7,9 @@ from models.dto.profile import Profile
 class Story(BaseModel):
     id: int
     profile_id: int
-    session_id: str
+    ws_session_uid: str
     profile: Profile
-    title: str
+    title: Optional[str]
     features: Optional[str]
     synopsis: Optional[str]
     last_successful_step: Optional[int]
