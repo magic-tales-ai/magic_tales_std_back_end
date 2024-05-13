d0e8436ba32606ad8dde6257c22c9bd6813fc5be
KevinSieb <kevinsiebenhar7@gmail.com>
2024-05-13 16:45:26 -0300

    feat: delete scripts

--- database/script_2024-04-11.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE users ADD active TINYINT DEFAULT 0 AFTER validation_code;
-UPDATE users SET active = 1; /* ACTIVATE ALL USERS */
--- database/script_2024-05-02.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE stories
-rename COLUMN session_id  to ws_session_uid;
-
-ALTER TABLE conversations
-rename COLUMN session_id  to ws_session_uid;
